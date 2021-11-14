f08434ba57b1ba3aad0601c370b708e9b388b878
tiny Flutter team <doanthanhduong11031990@gmail.com>
2021-11-14 07:38:06 +0700

    update url for static. media upload

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
@@ -9,7 +7,3 @@ urlpatterns = [
     path('banners', views.banners, name='banners'),
     path('voucher/list', views.getListVoucher, name='getListVoucher'),
 ] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
