1c8ce3bbc7a65591021099e8381838dcced03d59
tiny Flutter team <doanthanhduong11031990@gmail.com>
2021-11-14 08:34:24 +0700

    add thumbnail for category admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 # Django Libs:
 from django.contrib import admin
 from django import forms
+from django.utils.html import format_html
 
 # Local Libs:
 from .models import AdBanner, Buyer, BuyerAddress, BuyerLoginInfo, BuyerVoucher, Driver, DriverOrderRating, Merchant, MerchantBuyerFavorite, MerchantCategory, MerchantVoucher, Order, OrderProduct, Product, ProductGroup, Voucher
@@ -41,7 +42,45 @@ admin.site.register(Merchant, MerchantAdmin)
 
 admin.site.register(MerchantBuyerFavorite)
 
-admin.site.register(MerchantCategory)
+
+class MerchantCategoryAdminForm(forms.ModelForm):
+    class Meta:
+        model = MerchantCategory
+        widgets = {
+            'name': forms.TextInput(attrs={'size': "50"})
+        }
+        fields = '__all__'
+
+class MerchantCategoryAdmin(admin.ModelAdmin):
+    # Thumbnail
+    # https://sorl-thumbnail.readthedocs.io/en/latest/examples.html#admin-examples
+
+    def thumbnail_preview(self, obj):
+        return format_html('<img src="{}" width="auto" height="64px" />'.format(obj.thumbUrl.url))
+    thumbnail_preview.short_description = 'Thumbnail preview'
+    
+    def thumbnail_preview_detail(self, obj):
+        return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.thumbUrl.url))
+    thumbnail_preview_detail.short_description = 'Thumbnail preview'
+
+    def edit_button(self, obj):
+        return format_html('<a class="btn" href="/admin/api/merchantcategory/{}/change/"><input type="submit" value="Edit"></a>', obj.id)
+    edit_button.short_description = 'Edit'
+
+    def delete_button(self, obj):
+        return format_html('<a class="btn" href="/admin/api/merchantcategory/{}/delete/"><input type="submit" value="Delete"></a>', obj.id)
+    delete_button.short_description = 'Delete'
+    
+    model = MerchantCategory
+    # Using for list category display
+    list_display = ['id', 'name', 'thumbnail_preview', 'edit_button', 'delete_button']
+    # Using for detail display
+    readonly_fields = ['thumbnail_preview_detail']
+    list_display_links = ['id', 'thumbnail_preview', "name"]
+    form = MerchantCategoryAdminForm
+
+
+admin.site.register(MerchantCategory, MerchantCategoryAdmin)
 
 admin.site.register(Order)
 
